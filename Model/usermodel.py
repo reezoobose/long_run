2af1018931f23a7aa0e61307d7602ac19868d75b
reezoobose <reezoobose@rediffmail.com>
2018-10-27 21:03:58 +0600

    Bug Fixed

--- Model/usermodel.py
+++ Model/usermodel.py
@@ -129,12 +129,12 @@ class UserModel(db.Model):
         elif safe_str_cmp(leader_board_name, "Businessman"):
             upper_limit = 1000001
             lower_limit = 5000000
-            user_leader_board_name = 'BigMan'
+            user_leader_board_name = 'Businessman'
             user_leader_board_list = cls.query.filter(cls.us_dollar.between(lower_limit, upper_limit))
         elif safe_str_cmp(leader_board_name, "Entrepreneur"):
             upper_limit = 500000000
             lower_limit = 5000001
-            user_leader_board_name = 'Businessman'
+            user_leader_board_name = 'Entrepreneur'
             user_leader_board_list = cls.query.filter(cls.us_dollar.between(lower_limit, upper_limit))
         elif safe_str_cmp(leader_board_name, "Tycoon"):
             upper_limit = 2000000000
